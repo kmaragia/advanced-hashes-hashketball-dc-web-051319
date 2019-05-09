7dcd6e24064d4ce70b7336a6096fe8eabbd40056
Kemunto Maragia <kmaragia@email.wm.edu>
2019-05-09 23:17:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -120,7 +120,7 @@ def num_points_scored(name)
   hash.each do |location, info| 
     info.each do |attribute, stuff| 
       if stuff.include?(name) 
-        return hash[location][attribute][name][:points]
+        puts hash[location][attribute][name][:points]
       end
     end
   end
